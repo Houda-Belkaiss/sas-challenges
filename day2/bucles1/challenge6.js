151e4b7cdcd017097036789cbc5a916f089f6a20
Houda-Belkaiss <d2023houda@gmail.com>
2026-09-02 14:54:07 +0100

    challenge6

--- day2/bucles1/challenge6.js
+++ day2/bucles1/challenge6.js
@@ -1,3 +1,25 @@
-var prompt = require('prompt-sync')();
-let depart = prompt('Départ :');
+// Challenge 6 : Compte à rebours
+// Écrivez un programme qui effectue un compte à rebours à partir d'un nombre donné jusqu'à 0.
+// Exemple :
+// Départ : 5
+// 5
+// 4
+// 3
+// 2
+// 1
+// 0
+// Décollage !
+// Utilisez while.
+let prompt = require('prompt-sync')();
+
+let n = prompt('Entrez un nomber:');
+console.log('Départ :',n);
+
+
+while(n >= 0){
+    console.log(n);
+    n--;
+}
+    
+
 
