778e450e9cf5e0cc40baf300021e60599c277ff4
Houda-Belkaiss <d2023houda@gmail.com>
2026-09-03 15:36:35 +0100

    add chalanges

--- day3/Fonctions/challenge7.js
+++ day3/Fonctions/challenge7.js
@@ -1,3 +1,13 @@
-function obtenirHeureActuelle(){
-    return 
-}
+
+function obtenirHeureActuelle(){  
+        let d =  new Date();
+        let hours = d.getHours();
+        let minutes = d.getMinutes();
+        let seconds = d.getSeconds();
+        
+        
+       return    `${hours}:${minutes}:${seconds}`;
+    
+ }
+
+console.log(obtenirHeureActuelle());
