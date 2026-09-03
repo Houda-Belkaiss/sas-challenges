80694a90d60e336f30feafc91a8a948dbb5b1077
Houda-Belkaiss <d2023houda@gmail.com>
2026-09-03 10:24:19 +0100

    challenge3

--- day3/Fonctions/challenge3.js
+++ day3/Fonctions/challenge3.js
@@ -1,7 +1,8 @@
 let prompt = require('prompt-sync')();
 let nom = prompt('nom:');
-let prenom = prompt('prenom');
+let prenom = prompt('prenom:');
 
 function genererEmail( prenom ,nom){
-      
+      return prenom + nom +"@entreprise.com";
 }
+console.log(genererEmail(prenom,nom));
