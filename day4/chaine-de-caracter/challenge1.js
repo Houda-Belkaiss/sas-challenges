955d36ffc9fc581db7fcb6ad81058fc031abb2f9
Houda-Belkaiss <d2023houda@gmail.com>
2026-09-04 15:10:31 +0100

    challenge1

---
+++ day4/chaine-de-caracter/challenge1.js
@@ -0,0 +1,12 @@
+
+
+function chaineCaracteres(chaine){
+       let somme = 0;
+       for(let i=0; i< chaine;i++ ){
+        somme = somme + 1;
+       }
+       return somme;
+       
+}
+
+ console.log(chaineCaracteres("code"));
